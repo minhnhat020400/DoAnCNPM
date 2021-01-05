b34d3323e2480e6e4510d4db9a373aaf15a1bb73
DESKTOP-ODM8L1O\Nhat <nguyenminhnhat020400@gmail.com>
2021-01-05 10:29:49 +0700

    5/1/2021

--- MyHabit/FormThongKetuan.Designer.cs
+++ MyHabit/FormThongKetuan.Designer.cs
@@ -30,39 +30,176 @@ namespace MyHabit
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormThongKetuan));
-            this.btnHome = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDiscover = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnWep = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabEat = new System.Windows.Forms.TabPage();
+            this.tabDrink = new System.Windows.Forms.TabPage();
+            this.tabSleep = new System.Windows.Forms.TabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btnHome
+            // pictureBox1
             // 
-            this.btnHome.BackColor = System.Drawing.Color.Transparent;
-            this.btnHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnHome.Image = ((System.Drawing.Image)(resources.GetObject("btnHome.Image")));
-            this.btnHome.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnHome.Location = new System.Drawing.Point(667, 11);
-            this.btnHome.Margin = new System.Windows.Forms.Padding(2);
-            this.btnHome.Name = "btnHome";
-            this.btnHome.Size = new System.Drawing.Size(132, 45);
-            this.btnHome.TabIndex = 14;
-            this.btnHome.Text = "Home";
-            this.btnHome.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnHome.UseVisualStyleBackColor = false;
-            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1050, 106);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.btnDiscover);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.btnWep);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 106);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(251, 462);
+            this.panel1.TabIndex = 7;
+            // 
+            // btnDiscover
+            // 
+            this.btnDiscover.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnDiscover.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDiscover.Image = ((System.Drawing.Image)(resources.GetObject("btnDiscover.Image")));
+            this.btnDiscover.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDiscover.Location = new System.Drawing.Point(0, 257);
+            this.btnDiscover.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDiscover.Name = "btnDiscover";
+            this.btnDiscover.Size = new System.Drawing.Size(251, 76);
+            this.btnDiscover.TabIndex = 4;
+            this.btnDiscover.Text = "Khám Phá";
+            this.btnDiscover.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnDiscover.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(0, 370);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(251, 70);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "BMI";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // btnWep
+            // 
+            this.btnWep.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnWep.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWep.Image = ((System.Drawing.Image)(resources.GetObject("btnWep.Image")));
+            this.btnWep.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnWep.Location = new System.Drawing.Point(0, 36);
+            this.btnWep.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnWep.Name = "btnWep";
+            this.btnWep.Size = new System.Drawing.Size(251, 74);
+            this.btnWep.TabIndex = 3;
+            this.btnWep.Text = "Trang Chủ";
+            this.btnWep.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnWep.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(0, 144);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(251, 78);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Tôi";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabEat);
+            this.tabControl1.Controls.Add(this.tabDrink);
+            this.tabControl1.Controls.Add(this.tabSleep);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(251, 106);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(799, 462);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabEat
+            // 
+            this.tabEat.Location = new System.Drawing.Point(4, 25);
+            this.tabEat.Name = "tabEat";
+            this.tabEat.Padding = new System.Windows.Forms.Padding(3);
+            this.tabEat.Size = new System.Drawing.Size(791, 433);
+            this.tabEat.TabIndex = 0;
+            this.tabEat.Text = "Ăn";
+            this.tabEat.UseVisualStyleBackColor = true;
+            // 
+            // tabDrink
+            // 
+            this.tabDrink.Location = new System.Drawing.Point(4, 25);
+            this.tabDrink.Name = "tabDrink";
+            this.tabDrink.Padding = new System.Windows.Forms.Padding(3);
+            this.tabDrink.Size = new System.Drawing.Size(791, 433);
+            this.tabDrink.TabIndex = 1;
+            this.tabDrink.Text = "uống";
+            this.tabDrink.UseVisualStyleBackColor = true;
+            // 
+            // tabSleep
+            // 
+            this.tabSleep.Location = new System.Drawing.Point(4, 25);
+            this.tabSleep.Name = "tabSleep";
+            this.tabSleep.Padding = new System.Windows.Forms.Padding(3);
+            this.tabSleep.Size = new System.Drawing.Size(791, 433);
+            this.tabSleep.TabIndex = 2;
+            this.tabSleep.Text = "ngủ";
+            this.tabSleep.UseVisualStyleBackColor = true;
             // 
             // FormThongKetuan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnHome);
+            this.ClientSize = new System.Drawing.Size(1050, 568);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormThongKetuan";
             this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDiscover;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnWep;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabEat;
+        private System.Windows.Forms.TabPage tabDrink;
+        private System.Windows.Forms.TabPage tabSleep;
     }
 }
